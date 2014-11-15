2c8d42fe1a2aa5b37aa6484da159c8970c53010f
Massimo <massimo@rigel.biz>
2014-11-15 14:04:22 +0100

    added a test class

---
+++ Git Test/CCTest.h
@@ -0,0 +1,13 @@
+//
+//  CCTest.h
+//  Git Test
+//
+//  Created by Massimo Bonucchi on 15/11/14.
+//  Copyright (c) 2014 Rigel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CCTest : NSObject
+
+@end
---
+++ Git Test/CCTest.m
@@ -0,0 +1,13 @@
+//
+//  CCTest.m
+//  Git Test
+//
+//  Created by Massimo Bonucchi on 15/11/14.
+//  Copyright (c) 2014 Rigel. All rights reserved.
+//
+
+#import "CCTest.h"
+
+@implementation CCTest
+
+@end
